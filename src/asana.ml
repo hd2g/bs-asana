563115542f78f998b4816ca6e9684cee8cd18588
s01um <s01um>
2021-07-19 22:18:35 +0900

    Edit asana.ml file.

--- src/asana.ml
+++ src/asana.ml
@@ -1,34 +1,52 @@
-type attachments
-type batch_api
-type custom_field_settings
-type custom_fields
-type events
-type jobs
-type organization_exports
-type portfolio_memberships
-type portfolios
-type project_memberships
-type project_statuses
-type projects
-type resource
-type sections
-type stories
-type tags
-type tasks
-type teams
-type typeahead
-type user_task_lists
-type users
-type webhooks
-type workspace_memberships
-type workspaces
+type methods = Get | Post | Put | Delete
 
-module Resource = struct
-  type t = resource
+module UriQuery = struct
+  let rec concat = function
+    | [] -> ""
+    | (key, value) :: [] -> key ^ "=" ^ value
+    | (key, value) :: tl -> key ^ "=" ^ value ^ "&" ^ concat tl
 
-  external default_page_limit : int = "DEFAULT_PAGE_LIMIT" [@@bs.module "resource"]
+  let add query uri = uri ^ "?" ^ concat query
 end
 
-module Projects = struct
-  type t = projects
+module Client = struct
+  type members_t =
+    { instance : Axios.Instance.t
+    ; url : string
+    ; methods : methods
+    }
+
+  let base_url = "https://app.asana.com/api/1.0"
+
+  let makeInstance ?(baseUrl = base_url) token =
+    let headers =
+      [ "Content-Type", "application/json"
+      ; "Authorication", "Bearer " ^ token
+      ]
+      |> Js.Dict.fromList
+      |> Axios.Headers.fromDict
+    in
+    let config = Axios.makeConfig ~headers:headers ~baseURL:baseUrl () in
+    Axios.Instance.create config
+
+  let build personalAccessToken ?(query = []) uri methods =
+    let instance = makeInstance personalAccessToken in
+    let url = UriQuery.add query uri in
+    { instance; url; methods }
+
+  module Users = struct
+    type t
+
+    let get builder = builder "/users" Get
+    let me builder = builder "/users/me" Get
+    let get_user userId builder = builder "/users/" ^ userId
+  end
 end
+
+(*
+let () =
+  let token = "" in
+  let client = Asana.Client.create () in
+  let instance = Asana.Client.useAccessToken client token in
+  let me = Asana.Client.Users.me client in
+ *)
